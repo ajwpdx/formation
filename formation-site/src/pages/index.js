6f3eae96f36ace4c933f46191a34cba39c5f4e8f
Alex Williams <alexjwilliams1@gmail.com>
2021-02-21 12:04:55 -0800

    added shape grid and nav

--- formation-site/src/pages/index.js
+++ formation-site/src/pages/index.js
@@ -1,11 +1,36 @@
 import React from "react"
 import '../styles/index.scss'
 import Header from '../components/header'
+import triangle from '../images/formation-triangle.png'
+import square from '../images/formation-square.png'
+import circle from '../images/formation-circle.png'
 
 const IndexPage = () => (
-  
-  <div className='main-page'>
-    <h1>FORMATION</h1>
+
+  <div id='home'>
+
+<header>
+    <nav>
+    <a>RULES</a>
+    <a>PRINT & PLAY</a>
+    <a>BUY</a>
+    <a>CONTACT</a>
+    </nav>
+</header>
+
+    {/* <h1>FORMATION</h1> */}
+    <div className='shape-grid'>
+      <img src={triangle} className='shape triangle' />
+      <img src={triangle} className='shape triangle' />
+      <img src={triangle} className='shape triangle' />
+      <img src={square} className='shape square' />
+      <img src={square} className='shape square' />
+      <img src={square} className='shape square' />
+      <img src={circle} className='shape circle' />
+      <img src={circle} className='shape circle' />
+      <img src={circle} className='shape circle' />
+      <h3>a card-arranging strategy game</h3>
+    </div>
   </div>
 )
 
